e846a36b97b4f1447c0bca02124c3a912bfdf2cc
Chris Keogh <chris.keogh@xero.com>
2021-05-09 17:16:23 +1200

    add identityserver + mvc client scaffold

---
+++ README.md
@@ -0,0 +1,12 @@
+### Passwordless Auth Example
+
+Using [IdentityServer4](https://github.com/IdentityServer/IdentityServer4), dotnet core, [WebAuthN](https://webauthn.io/) and [FIDO2.NET](https://github.com/abergs/fido2-net-lib) this project aims to demonstrate just how simple
+it is to implement.
+
+For info on how WebAuthN works:
+[https://webauthn.guide/](https://webauthn.guide/)
+[https://www.okta.com/sites/default/files/pdf/How_WebAuthn_Works_0.pdf](https://www.okta.com/sites/default/files/pdf/How_WebAuthn_Works_0.pdf)
+
+For info on why we should stop using passwords:
+[https://www.okta.com/sites/default/files/2020-07/Okta_Passwordless_Report_Anglicised_Version_0.pdf](https://www.okta.com/sites/default/files/2020-07/Okta_Passwordless_Report_Anglicised_Version_0.pdf)
+[https://www.okta.com/resources/whitepaper/move-beyond-passwords/](https://www.okta.com/resources/whitepaper/move-beyond-passwords/)
---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+    }
+    
+    
+}
